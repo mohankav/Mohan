c643a521c184d996a9447a424890177b28a7ce4e
mohankav <kavimohan2498@gmail.com>
2023-09-20 14:12:28 +0530

    arrays

---
+++ Arrays.java
@@ -0,0 +1,34 @@
+package org.basic;
+
+public class Arrays {
+	
+	
+	public static void main(String[] args) {
+		
+		int a[]= {12,14,14,44};
+		
+		String[] S= {"Suresh","Selenium","project","training"};
+		
+		
+		System.err.println("Interger of Arrays");
+		
+		// by using for each loop
+		
+		
+		for(int X : a) {
+			System.out.println("The values are "+X);
+		}
+		
+		System.err.println("String of Arrays");
+		
+		// by using for each loop
+		
+		for(String Y: S) {
+			System.out.println(" The Word are "+Y);
+		}
+		
+		
+		
+	}
+
+}
